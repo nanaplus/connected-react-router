76285cfd70151271e19a91c65fe1d89ae2ff81bc
Francisco Lourenco <aristidesfl@gmail.com>
2018-07-23 18:04:30 +0200

    Make initial location change PUSH instead of POP

--- src/ConnectedRouter.js
+++ src/ConnectedRouter.js
@@ -58,7 +58,7 @@ const createConnectedRouter = (structure) => {
       // Listen to history changes
       this.unlisten = props.history.listen(handleLocationChange)
       // Dispatch a location change action for the initial location
-      handleLocationChange(props.history.location, props.history.action)
+      handleLocationChange(props.history.location, "PUSH")
     }
 
     componentWillUnmount() {
